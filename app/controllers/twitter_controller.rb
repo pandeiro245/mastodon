08e359a615cd8a4f38297278be0b9d6b7576333c
西小倉宏信 <nishiko@mindia.jp>
2017-04-14 18:19:54 +0000

    save avatar_remote_url from twitter

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -8,8 +8,9 @@ class TwitterController < ApplicationController
       @user.account = Account.new(username: data[:info][:nickname])
       @user.password  = Devise.friendly_token[0,20]
       @user.skip_confirmation!
-      @user.save!
     end
+    @user.account.avatar_remote_url = data[:info][:image]
+    @user.save!
     sign_in @user
     redirect_to :root
   end
