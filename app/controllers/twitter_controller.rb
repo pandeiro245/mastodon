c821d74f6b5e8302b9aa652be22edc884aae2e67
西小倉宏信 <nishiko@mindia.jp>
2017-04-14 16:16:05 +0000

    update twitter#callback for sign_in and sign_up

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,5 +1,16 @@
 class TwitterController < ApplicationController
   def callback
-    raise request.env["omniauth.auth"].inspect
+    data = request.env["omniauth.auth"]
+    @user = User.find_or_create_by(
+      email: "#{data['uid']}@#{request.domain}",
+    )
+    unless @user.account.present?
+      @user.account = Account.new(username: data[:info][:nickname])
+      @user.password  = Devise.friendly_token[0,20]
+      @user.skip_confirmation!
+      @user.save!
+    end
+    sign_in @user
+    redirect_to :root
   end
 end
